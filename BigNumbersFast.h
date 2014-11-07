b4f1a61dee101682c7638180cf930f708745893c
seanauff <seanauff@users.noreply.github.com>
2014-11-07 15:24:05 -0500

    Update BigNumbersFast.h

--- BigNumbersFast.h
+++ BigNumbersFast.h
@@ -30,7 +30,7 @@ class BigNumbersFast
     BigNumbersFast(LiquidCrystalFast*);
     void clearLargeNumber(byte);
     void displayLargeNumber(byte, byte);
-	  void displayLargeInt(int, byte, byte, bool);
+    void displayLargeInt(int, byte, byte, bool);
   private:
     LiquidCrystalFast* _lcd;
 };
